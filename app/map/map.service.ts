e1e582afb67b480f64a5d9b8e251c6a1f1b8616f
lnknguyen <luongnguyenkhoinguyen14@gmail.com>
2016-11-13 01:58:26 +0200

    change currencty for checkout

--- app/map/map.service.ts
+++ app/map/map.service.ts
@@ -64,6 +64,7 @@ export class MapService{
         var handler = (<any>window).StripeCheckout.configure({
             key: 'pk_test_cq1ut4ba4Ftin2AAUVEGnRbn',
             locale: 'auto',
+            currency: 'eur',
             token:  (token: any) => {
         // You can access the token ID with `token.id`.
         // Get the token ID to your server-side code for use.
