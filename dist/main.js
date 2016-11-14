ea0e325567c353efc702dae1f7bc2e09b4ce99d2
Danh Phan <final_fantasy_1995@yahoo.com.vn>
2016-11-14 14:46:03 +0200

    add alert

--- dist/main.js
+++ dist/main.js
@@ -59,6 +59,7 @@ function showCurlCommand(mergedEndpoint) {
   var curlCommand = 'curl --header "Authorization: key=' + API_KEY +
     '" --header Content-Type:"application/json" ' + GCM_ENDPOINT +
     ' -d "{\\"registration_ids\\":[\\"' + subscriptionId + '\\"]}"';
+    alert(curlCommand);
     console.log(curlCommand);
 }
 
