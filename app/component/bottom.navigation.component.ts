7138286814e84c8ea9f015fb10da707a69b25c79
Danh Phan <final_fantasy_1995@yahoo.com.vn>
2016-11-17 12:15:28 +0200

    merge with local

--- app/component/bottom.navigation.component.ts
+++ app/component/bottom.navigation.component.ts
@@ -14,31 +14,31 @@ import {BlackOverlay} from '../component/blackoverlay.component';
   <ul>
   <li routerLink="/parking" routerLinkActive="active">
   <div>
-  <img src="img/Free Parking-50.png" alt="parking zone"  class="custom-img-responsive"><br>
+  <img src="img/Free Parking-50.png" alt="parking zone"  class="custom-img-responsive">
   <span>Parking</span>
   </div>
   </li>
   <li routerLink="/hri" routerLinkActive="active">
   <div>
-  <img src="img/hri.png" alt="free parking" class="custom-img-responsive"><br>
+  <img src="img/hri.png" alt="free parking" class="custom-img-responsive">
   <span>HRI Data</span>
   </div>
   </li>
   <li routerLink="/parkandride" routerLinkActive="active">
   <div>
-  <img src="img/Park Ride-50.png" alt="park ride"  class="custom-img-responsive"><br>
+  <img src="img/Park Ride-50.png" alt="park ride"  class="custom-img-responsive">
   <span>Park & Ride</span>
   </div>
   </li>
   <li routerLink="/bike" routerLinkActive="active">
   <div>
-  <img src="img/Bicycle-50.png" alt="bike icon"  class="custom-img-responsive"><br>
+  <img src="img/Bicycle-50.png" alt="bike icon"  class="custom-img-responsive">
   <span>City Bike</span>
   </div>
   </li>
   <li routeLink="/user" routerLinkActive="active" id="testImg" #testImg>
   <div>
-  <img  src="img/User-Info.png" alt="user info"  class="custom-img-responsive"><br>
+  <img  src="img/User-Info.png" alt="user info"  class="custom-img-responsive">
   <span>User Info</span>
   </div>
   </li>
